c5e7d6cac247078e39ad956b1a14de60b6381b33
tentacion18 <122818207+tentacion18@users.noreply.github.com>
2023-07-25 22:54:17 +0530

    final

---
+++ final web tech 2/app.js
@@ -0,0 +1,45 @@
+const canvas = document.querySelector(".canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const context = canvas.getContext("2d");
+const frameCount = 274;
+let gojo = {frame : 0};
+
+const currentFrame = (index) => `./gojo/${(index + 1).toString()}.png`;
+const images = [];
+
+for (let i = 0; i < frameCount; i++) {
+    const img = new Image();
+    img.src = currentFrame(i);
+    images.push(img);
+}
+
+
+gsap.to(gojo, {
+    frame: frameCount - 1,
+    snap: "frame",
+    ease: "none",
+    scrollTrigger:{
+        scrub: 0.15,
+        trigger: '#page>.canvas',
+        start: 'top top',
+        pin: "canvas",
+        end : "200% top",  
+       
+           
+    },
+    onUpdate: render,
+})
+
+images [0].onload = render;
+
+function render() {
+    context.canvas.width = images[0].width;
+    context.canvas.height = images[0].height;
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(images[gojo.frame], 0, 0);
+    
+  }
+  
